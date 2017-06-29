8e3218714ac2963ebcb3e99b0b34a046c7a72b4d
Taoh Green <greenmachine777@gmail.com>
2017-06-29 15:42:56 -0700

    added warning for consoles

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {   
-    "globals": {
-        "toastr": true,
-        "React": true
-    },
-    "extends": ["react-app"],
-    "rules": {
-    	"no-extra-semi": 0
-    }
+  "globals": {
+    "toastr": true,
+    "React": true
+  },
+  "extends": ["react-app"],
+  "rules": {
+  	"no-extra-semi": 0,
+  	"no-console": [1, { allow: ["warn", "error"] }]
+  }
 }
