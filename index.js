b4d5a2664dd34f44abca8fc5fa4656bcf69a1275
Taoh Green <taohgreen@gmail.com>
2017-08-15 15:46:22 -0700

    update eslint rules

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 module.exports = {   
   "globals": {
-    "toastr": true,
-    "React": true
+    "toastr": true
   },
   "extends": ["react-app"],
-  "rules": {
-  	"no-extra-semi": 0,
-  	"no-console": [1, { allow: ["warn", "error"] }]
+  rules: {
+    'no-else-return': 1,
+    'react/jsx-boolean-value': [1, 'never'],
+    'no-console': [1, { allow: ['warn', 'error'] }],
+    'no-debugger': 2,
+    'no-empty': [2, { "allowEmptyCatch": true }],
+    'no-extra-boolean-cast': 2,
+    'no-extra-semi': 1,
+    'no-inner-declarations': [2, 'functions'],
+    'no-undef': 2,
+    'no-unneeded-ternary': 1,
+    'no-unsafe-negation': 2,
+    'no-unused-vars': [
+      1,
+      { ignoreRestSiblings: true }
+    ],
+    'no-var': 1,
+    'object-shorthand': 1
   }
 }
