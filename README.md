66f5ab83bcb2954dcb559386ee294e9180247fa6
tnrich <tnrich@teselagen.com>
2019-08-19 13:31:15 -0700

    adding cypress config

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
 # Teselagen's shared linting config
 
-
 ## Installation
+
 requires v6 of eslint or higher
 1. `yarn add -D eslint eslint-config-teselagen`
 
-(select yes to install using yarn when prompted)
-
-
 2. Add the following to `.eslintrc.js`
+
 ```
 require("eslint-config-teselagen/patch");
 module.exports = {
-	extends: "teselagen/react" //choose one of these 
-	extends: "teselagen/node" //choose one of these 
-	extends: "teselagen/es5" //choose one of these 
+  //choose one of the following:
+	extends: "teselagen/react"
+	extends: "teselagen/node"
+	extends: "teselagen/cypress"
+	extends: "teselagen/es5"
 }
 ```
 
 For React.js projects, extend from `teselagen/react`
 For Node.js projects, extend from `teselagen/node`
 
-
-
 ## Adding linting on precommit (recommended!)
 
 Then add this to your package.json:
+
 ```
 "scripts": {
   "precommit": "lint-staged"
--- cypress.js
+++ cypress.js
@@ -1,45 +1,15 @@
-const path = require('path');
-
-let currentModule = module;
-while (!/[\\/]eslint[\\/]lib[\\/]cli-engine[\\/]config-array-factory\.js/i.test(currentModule.filename)) {
-  if (!currentModule.parent) {
-    // This was tested with ESLint 6.1.0; other versions may not work
-    throw new Error('Failed to patch ESLint because the calling module was not recognized');
-  }
-  currentModule = currentModule.parent;
-}
-const eslintFolder = path.join(path.dirname(currentModule.filename), '../..');
-
-const configArrayFactoryPath = path.join(eslintFolder, "lib/cli-engine/config-array-factory");
-const configArrayFactoryModule = require(configArrayFactoryPath);
-
-const moduleResolverPath = path.join(eslintFolder, "lib/shared/relative-module-resolver");
-const ModuleResolver = require(moduleResolverPath);
-
-const originalLoadPlugin = configArrayFactoryModule.ConfigArrayFactory.prototype._loadPlugin;
-configArrayFactoryModule.ConfigArrayFactory.prototype._loadPlugin = function(name, importerPath, importerName) {
-  const originalResolve = ModuleResolver.resolve;
-  try {
-    ModuleResolver.resolve = function (moduleName, relativeToPath) {
-      // resolve using importerPath instead of relativeToPath
-      return originalResolve.call(this, moduleName, importerPath);
-    };
-    return originalLoadPlugin.apply(this, arguments);
-  } finally {
-    ModuleResolver.resolve = originalResolve;
-  }
-}
 module.exports = {
-  "plugins": ["cypress", "no-only-tests", "chai-friendly"],
-  "extends": ["plugin:cypress/recommended"],
+  "plugins": [
+    "cypress",
+    "no-only-tests"
+  ],
+  "extends": [
+    "plugin:cypress/recommended"
+  ],
   "env": {
     "cypress/globals": true
   },
   "rules": {
-    "no-only-tests/no-only-tests": 1,
-    "no-unused-expressions": 0,
-    "chai-friendly/no-unused-expressions": 2
+    "no-only-tests/no-only-tests": 1
   }
 }
-
-
