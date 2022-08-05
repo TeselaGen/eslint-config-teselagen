619b8bfe15a8f9ce2f98913bde45dbac4743066b
Taoh Green <taohgreen@gmail.com>
2022-08-05 15:12:08 -0700

    Add fix true to no-only

--- cypress.js
+++ cypress.js
@@ -1,17 +1,12 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "plugins": [
-    "cypress",
-    "no-only-tests"
-  ],
-  "extends": [
-    "plugin:cypress/recommended"
-  ],
-  "env": {
-    "cypress/globals": true
+  plugins: ["cypress", "no-only-tests"],
+  extends: ["plugin:cypress/recommended"],
+  env: {
+    "cypress/globals": true,
   },
-  "rules": {
-    "no-only-tests/no-only-tests": 1
-  }
-}
+  rules: {
+    "no-only-tests/no-only-tests": [1, { fix: true }],
+  },
+};
