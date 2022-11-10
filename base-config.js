87d4c759fc4360a3a907023c4cfddafa005ac9d0
Taoh Green <taohgreen@gmail.com>
2022-11-10 11:08:06 -0800

    add functions exception

--- base-config.js
+++ base-config.js
@@ -3,6 +3,9 @@ module.exports = {
     "no-unused-vars": 0,
     "@typescript-eslint/no-unused-vars": [1, { ignoreRestSiblings: true }],
     "no-use-before-define": 0,
-    "@typescript-eslint/no-use-before-define": 1,
+    "@typescript-eslint/no-use-before-define": [
+      1,
+      { functions: false, classes: false },
+    ],
   },
 };
